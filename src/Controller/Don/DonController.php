039057556abe8984a81d95c3d24f9bbf0d056a34
bamba-salim <01salim.bamba@gmail.com>
2021-06-23 17:07:22 +0200

    donnations:  - crée les classes -> ok  - creée les vu -> ok  - afficher le lien vers les routes -> todo

--- src/Controller/Don/DonController.php
+++ src/Controller/Don/DonController.php
@@ -2,17 +2,41 @@
   
   namespace App\Controller\Don;
   
+  use App\Entity\Don;
   use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
   use Symfony\Component\HttpFoundation\Response;
   use Symfony\Component\Routing\Annotation\Route;
   
+  /** @Route("/donnations") */
   class DonController extends AbstractController
   {
-    /** @Route("/donnations", name="don") */
+    /** @Route("/", name="don") */
     public function index(): Response
     {
+      $lastDon = $this->getDoctrine()->getRepository(Don::class)->fetchLastDon();
       return $this->render('don/index.html.twig', [
           'controller_name' => 'DonController',
+          'lastDon' => $lastDon
+      ]);
+    }
+  
+    /** @Route("/list", name="all_dons") */
+    public function dons(): Response
+    {
+      $donList = $this->getDoctrine()->getRepository(Don::class)->fetchDonList();
+      dd($donList);
+//      return $this->render('don/index.html.twig', [
+//          'controller_name' => 'DonController',
+//          'lastDon' => $lastDon
+//      ]);
+    }
+  
+    /** @Route("/{id}", name="single_don") */
+    public function singleDon($id): Response
+    {
+      return $this->render('don/single.html.twig', [
+          'controller_name' => 'DonController',
+          'don' => $this->getDoctrine()->getRepository(Don::class)->find($id)
       ]);
     }
   }
---
+++ src/Repository/DonRepository.php
@@ -0,0 +1,75 @@
+<?php
+  
+  namespace App\Repository;
+  
+  use App\Entity\Don;
+  use App\Entity\User;
+  use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+  use Doctrine\ORM\NonUniqueResultException;
+  use Doctrine\ORM\NoResultException;
+  use Doctrine\Persistence\ManagerRegistry;
+  
+  /**
+   * @method Don|null find($id, $lockMode = null, $lockVersion = null)
+   * @method Don|null findOneBy(array $criteria, array $orderBy = null)
+   * @method Don[]    findAll()
+   * @method Don[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+   */
+  class DonRepository extends ServiceEntityRepository
+  {
+    public function __construct(ManagerRegistry $registry)
+    {
+      parent::__construct($registry, Don::class);
+    }
+    
+    
+    public function fetchLastDon(): Don
+    {
+      return $this->findOneBy([], ['date_transaction' => 'ASC']);
+    }
+    
+    public function fecthDonById(int $idDon): Don
+    {
+      return $this->find($idDon);
+    }
+    
+    public function fetchDonList(): array
+    {
+      //todo: pagination
+      return $this->createQueryBuilder('d')
+          ->orderBy('d.date_transaction', 'DESC')
+          ->getQuery()
+          ->getResult();
+    }
+    
+    
+    
+    // /**
+    //  * @return Don[] Returns an array of Don objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('d')
+            ->andWhere('d.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('d.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+    
+    /*
+    public function findOneBySomeField($value): ?Don
+    {
+        return $this->createQueryBuilder('d')
+            ->andWhere('d.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+  }
