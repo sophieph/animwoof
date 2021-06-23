0492c786cd4e17fb1783496e4d82687a040942ac
sophieph <so.phouangsy@gmail.com>
2021-06-23 23:02:45 +0200

    commande

--- src/Controller/Boutique/CommandeController.php
+++ src/Controller/Boutique/CommandeController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller\Boutique;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/commande")
+ * Class CommandeController
+ * @package App\Controller\Boutique
+ * @IsGranted("ROLE_USER")
+ */
 class CommandeController extends AbstractController
 {
-    #[Route('/boutique/commande', name: 'boutique_commande')]
+    /**
+     * @Route("/", name="commande")
+     * @return Response
+     */
     public function index(): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_USER');
         return $this->render('boutique/commande/index.html.twig', [
             'controller_name' => 'CommandeController',
         ]);
