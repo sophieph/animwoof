f2bde581d31962d9376c59f6550110fc7f8fb4b5
sophieph <so.phouangsy@gmail.com>
2021-06-24 12:33:01 +0200

    layout back-ofice

--- src/Controller/Blog/BlogController.php
+++ src/Controller/Blog/BlogController.php
@@ -6,9 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+/**
+ * @Route("/blog")
+ * Class BlogController
+ * @package App\Controller\Blog
+ */
 class BlogController extends AbstractController
 {
+    /**
+     * @Route("/", name="blog")
+     * @return Response
+     */
     public function index(): Response
     {
         return $this->render('blog/index.html.twig', [
